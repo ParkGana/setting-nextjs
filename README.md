103d7597fdbb17db2c47dc420854a34a3cc3f296
ParkGana <gnchoco97@gmail.com>
2023-11-29 02:25:59 +0900

    next.js 기초 설정

--- README.md
+++ README.md
@@ -18,3 +18,12 @@ npx create-next-app . --typescript
 \> Would you like to use `src/` directory with this project? ... No  
 \> Use App Router (recommended)? ... No  
 \> Would you like to customize the default import alias? ... No
+
+<br />
+
+##### [ next.js 프로젝트 기초 설정 ]
+
+npm install -D prettier eslint-plugin-prettier eslint-config-prettier  
+npm install -D @typescript-eslint/parser @typescript-eslint/eslint-plugin  
+npm install -D styled-components@5.3.5 styled-reset  
+npm install -D @types/styled-components
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '../src/configuration/theme'
+import { style as Style } from '../src/configuration/style'
+import '../src/configuration/preload-font.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider theme={theme}>
+            <Style />
+            <Head>
+                <link rel="shortcut icon" href="/favicon.ico" />
+                <title>REACT</title>
+            </Head>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    )
 }
